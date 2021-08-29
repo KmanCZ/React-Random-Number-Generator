166ca5c497f63550178e1573ff2937db29d7746f
Kristián Řehoř <kmancz.kiki@gmail.com>
2021-08-29 21:32:12 +0200

    Updates

--- static/js/main.f606d6f0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonprandom-number-generator"]=this["webpackJsonprandom-number-generator"]||[]).push([[0],{10:function(e,n,r){"use strict";r.r(n);var t=r(0),o=r.n(t),c=r(3),a=r.n(c),d=(r(8),r(1));var i=function(){return Object(d.jsx)("div",{children:"Hello World!"})};a.a.render(Object(d.jsx)(o.a.StrictMode,{children:Object(d.jsx)(i,{})}),document.getElementById("root"))},8:function(e,n,r){}},[[10,1,2]]]);
-//# sourceMappingURL=main.f606d6f0.chunk.js.map
