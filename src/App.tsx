16a047eb60305a9f0b0e4abe695852e58ef1cf64
Kristián Řehoř <kmancz.kiki@gmail.com>
2021-08-29 19:04:36 +0200

    Added primitive form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function App() {
-  return <div>Hello World!</div>;
+  return <Form />;
+}
+
+function Form() {
+  return (
+    <form>
+      <section>
+        <label htmlFor="min">Minimum </label>
+        <input type="number" name="min" id="min" />
+      </section>
+      <section>
+        <label htmlFor="max">Maximum </label>
+        <input type="number" name="max" id="max" />
+      </section>
+      <button type="submit">Generate</button>
+    </form>
+  );
 }
 
 export default App;
